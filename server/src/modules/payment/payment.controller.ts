d7da496b6026e93b853391a7a161e79e810804f5
PronDmytro <dmytropron5838@gmail.com>
2021-11-06 21:38:21 +0200

    bugfix(PaymentController): fix prefix and sevice name

--- server/src/modules/payment/payment.controller.ts
+++ server/src/modules/payment/payment.controller.ts
@@ -3,37 +3,37 @@ import {PaymentService} from './payment.service';
 import {CreatePaymentReqDto, UpdatePaymentReqDto} from './dto';
 import {PaymentEntity} from '../../entities/payment.entity';
 
-@Controller('position')
+@Controller('payment')
 export class PaymentController {
 
   public constructor(
-    private readonly positionService: PaymentService,
+    private readonly paymentService: PaymentService,
   ) {
   }
 
   @Get('')
   public findAll(): Promise<PaymentEntity[]> {
-    return this.positionService.findAll();
+    return this.paymentService.findAll();
   }
 
   @Get(':id')
   public findOne(@Param('id') id: string): Promise<PaymentEntity> {
-    return this.positionService.findOne(id);
+    return this.paymentService.findOne(id);
   }
 
   @Post('')
   public async create(@Body() createClientDto: CreatePaymentReqDto): Promise<PaymentEntity> {
-    return await this.positionService.create(createClientDto);
+    return await this.paymentService.create(createClientDto);
   }
 
   @Put()
   public async edit(@Body() data: UpdatePaymentReqDto) {
-    return await this.positionService.edit(data);
+    return await this.paymentService.edit(data);
   }
 
   @Delete(':id')
   public async delete(@Param('id') id: string) {
-    return await this.positionService.delete(id);
+    return await this.paymentService.delete(id);
   }
 
 }
